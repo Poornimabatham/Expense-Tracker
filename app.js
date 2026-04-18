0dbcb10e758d385ba15b4a3426289d6481942acd
Poornima Batham <pbatham21@gmail.com>
2026-04-18 17:42:16 +0530

    add code

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const connectDB = require('./config/db');
 const { errorHandler } = require('./middleware/errorMiddleware');
 const transactionRoutes = require('./routes/transactionRoutes');
 
 const app = express();
 
 app.use(express.json());
+
+app.use(async (req, res, next) => {
+  await connectDB();
+  next();
+});
+
 app.use('/api/transactions', transactionRoutes);
 app.use(errorHandler);
 
--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-const app = require('./app');
-const connectDB = require('./config/db');
 require('dotenv').config();
+const app = require('./app');
 
-const PORT = process.env.PORT || 5000;
-
-connectDB().then(() => {
+if (process.env.NODE_ENV !== 'production') {
+  const PORT = process.env.PORT || 5000;
   app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-});
+}
+
+module.exports = app;
